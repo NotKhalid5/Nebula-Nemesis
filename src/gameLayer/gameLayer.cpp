43f13f5830c6ae656f8b131b7c93bb1b266c9fa3
NotKhalid5 <khalidajr@icloud.com>
2026-03-04 22:30:28 -0500

    Loaded First spaceship texture (abt 2 make SS move)

---
+++ src/gameLayer/gameLayer.cpp
@@ -0,0 +1,61 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "gameLayer.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "platformInput.h"
+#include "imgui.h"
+#include <iostream>
+#include <sstream>
+#include "imfilebrowser.h"
+#include <gl2d/gl2d.h>
+#include <platformTools.h>
+
+
+
+gl2d::Renderer2D renderer;
+
+gl2d::Texture spaceShipTexture;
+
+bool initGame() {
+	// initializing renderer
+	gl2d::init();
+	renderer.create();
+
+	spaceShipTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true); // path specific; true param tells gpu u wanna keep the texture pixelated
+	
+	return true;
+}
+
+
+
+bool gameLogic(float deltaTime) {
+#pragma region init stuff
+	int w = 0; int h = 0;
+	w = platform::getFrameBufferSizeX(); // window w
+	h = platform::getFrameBufferSizeY(); // window h
+	
+	glViewport(0, 0, w, h);
+	glClear(GL_COLOR_BUFFER_BIT); // clear screen
+
+	renderer.updateWindowMetrics(w, h);
+#pragma endregion
+
+
+
+	renderer.renderRectangle({100,100, 100, 100}, spaceShipTexture);
+
+
+	renderer.flush(); // called once
+
+
+	//ImGui::ShowDemoWindow();
+
+
+	return true;
+#pragma endregion
+
+}
+
+// ts f(x) might not b called if prgm is forced closed
+void closeGame(){}
