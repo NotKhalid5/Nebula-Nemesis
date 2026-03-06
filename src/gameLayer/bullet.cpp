759c203253687c43b1a9ac7b5751b483d109b3f0
NotKhalid5 <khalidajr@icloud.com>
2026-03-06 06:31:48 -0500

    Success fully added bullets and matched bullet speed to ship light speed

---
+++ src/gameLayer/bullet.cpp
@@ -0,0 +1,13 @@
+#include <bullet.h>
+
+void Bullet::render(gl2d::Renderer2D &renderer, 
+    gl2d::Texture bulletsTexture, gl2d::TextureAtlasPadding bulletsAtlas) {
+    
+    renderer.renderRectangle({position + glm::vec2(25, 25), 50, 50}, 
+    bulletsTexture, Colors_White, {}, 0, bulletsAtlas.get(1, 1)); 
+
+}
+
+void Bullet::update(float deltaTime) {
+    position += fireDirection * deltaTime * 1999.f;
+}
--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -11,10 +11,14 @@
 #include <gl2d/gl2d.h>
 #include <platformTools.h>
 #include <tiledRenderer.h>
+#include <bullet.h>
+#include <vector>
 
 
 struct GameplayData { // place gameplay inside a struct so easier to reset the game correctly
 	glm::vec2 playerPos = {100, 100};
+
+	std::vector<Bullet> bullets;  
 };
 
 GameplayData data;
@@ -23,9 +27,13 @@ gl2d::Renderer2D renderer;
 
 constexpr int BACKGROUNDS = 4;
 
+// declare space ship texture and atlas
 gl2d::Texture spaceShipsTexture;
 gl2d::TextureAtlasPadding spaceShipsAtlas;
 
+// declare bullet texture and atlas
+gl2d::Texture bulletsTexture;
+gl2d::TextureAtlasPadding bulletsAtlas;
 // gl2d::Texture spaceShipTexture;
 
 gl2d::Texture backgroundTexture[BACKGROUNDS]; // subsequently make an arr of Texture obj 2, 2 correspond w/ TR objs
@@ -36,8 +44,13 @@ bool initGame() {
 	gl2d::init();
 	renderer.create();
 
+	// ships texture atlas
 	spaceShipsTexture.loadFromFileWithPixelPadding(RESOURCES_PATH "spaceShip/stitchedFiles/spaceships.png", 128, true);
 	spaceShipsAtlas = gl2d::TextureAtlasPadding(5, 2, spaceShipsTexture.GetSize().x, spaceShipsTexture.GetSize().y);
+
+	// bullets texture atlas
+	bulletsTexture.loadFromFileWithPixelPadding(RESOURCES_PATH "spaceShip/stitchedFiles/projectiles.png", 500, true);
+	bulletsAtlas = gl2d::TextureAtlasPadding(3, 2, bulletsTexture.GetSize().x, bulletsTexture.GetSize().y);
 
 	// spaceShipTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true); // path specific; true param tells gpu u wanna keep the texture pixelated
 	backgroundTexture[0].loadFromFile(RESOURCES_PATH "background1.png", true);
@@ -60,10 +73,10 @@ bool initGame() {
 	return true;
 }
 
-
+constexpr float shipSize = 250.f;
 
 bool gameLogic(float deltaTime) {
-#pragma region init stuff
+#pragma region init
 	int w = 0; int h = 0;
 	w = platform::getFrameBufferSizeX(); // window w
 	h = platform::getFrameBufferSizeY(); // window h
@@ -122,17 +135,22 @@ bool gameLogic(float deltaTime) {
 
 #pragma endregion
 
+
+#pragma region camera follow
+	renderer.currentCamera.follow(data.playerPos, deltaTime * 2000, 10, 200, w,h); // tracks plyr (I wanteed the camera speed to match the speed of the plyr's mvmnts)
+#pragma endregion 
+
+
 #pragma region render bg
 	renderer.currentCamera.zoom = 1.1; // changes how much the cam can c
 
 	for (int i = 0; i < BACKGROUNDS; i++) {
 		tiledRenderer[i].render(renderer); 
 	}
-
 #pragma endregion
 
-#pragma region mouse pos 
 
+#pragma region mouse pos 
 	glm::vec2 mousePos = platform::getRelMousePosition();
 	glm::vec2 screenCenter(w / 2.f, h / 2.f); // vec pointing 2 mid of scr
 
@@ -146,20 +164,33 @@ bool gameLogic(float deltaTime) {
 	}
 
 	float spaceShipAngle = atan2(mouseDirection.y, -mouseDirection.x);
-
 #pragma endregion
 
-#pragma region camera follow
-	renderer.currentCamera.follow(data.playerPos, deltaTime * 2000, 10, 200, w,h); // tracks plyr (I wanteed the camera speed to match the speed of the plyr's mvmnts)
-#pragma endregion 
+
+#pragma region handle bullets
+	if (platform::isLMousePressed()) {
+		Bullet b;
+		b.position = data.playerPos;
+		b.fireDirection = mouseDirection;
+
+		data.bullets.push_back(b);
+	}
+
+	for (auto &b : data.bullets) {b.update(deltaTime);}
+#pragma endregion
+
 
 #pragma region render ship
-	constexpr float shipSize = 250.f;
-
 	renderer.renderRectangle({data.playerPos - glm::vec2(shipSize/2, shipSize/2), 200, 200}, spaceShipsTexture,
-		Colors_White, {}, glm::degrees(spaceShipAngle) + 90.f, spaceShipsAtlas.get(1, 0)); // idx the right elm n the atlas
-
+		Colors_White, {}, glm::degrees(spaceShipAngle) + 90.f, spaceShipsAtlas.get(3, 0)); // idx the right elm n the atlas
+#pragma endregion
  
+
+#pragma region render bullets
+	for (auto &b : data.bullets) {b.render(renderer, bulletsTexture, bulletsAtlas);} // render all bullets using enhanced 4 loop
+#pragma endregion
+
+
 	renderer.flush(); // called once
 
 
